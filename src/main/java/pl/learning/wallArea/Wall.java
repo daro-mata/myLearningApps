7454b9fe25edae2ad01dddd6731eff0cf3852986
Darek <dariusz.matacz11@gmail.com>
2022-11-07 11:09:23 +0100

    added Wall class

---
+++ src/main/java/pl/learning/wallArea/Wall.java
@@ -0,0 +1,74 @@
+package pl.learning.wallArea;
+
+public class Wall {
+
+    public static void main(String[] args) {
+
+        Wall wall = new Wall(5,-1);
+        System.out.println("Area = " + wall.getArea());
+
+        wall.setHeight(3);
+        System.out.println("Height: " + wall.getHeight());
+        System.out.println("Width: " + wall.getWidth());
+        System.out.println("Area: " + wall.getArea());
+
+        wall.setWidth(-2);
+        System.out.println("Height: " + wall.getHeight());
+        System.out.println("Width: " + wall.getWidth());
+        System.out.println("Area: " + wall.getArea());
+
+        Wall wall2 = new Wall();
+
+    }
+
+    private double width;
+    private double height;
+
+    public Wall (){
+    }
+
+    public Wall (double width, double height){
+        if(height<0){
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+        if (width<0){
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+
+    }
+
+    public double getWidth(){
+        return width;
+    }
+
+    public double getHeight(){
+        return height;
+    }
+
+    public void setWidth(double width){
+        if (width<0){
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+    }
+
+    public void setHeight(double height) {
+        if(height<0){
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
+    public double getArea (){
+        return this.height*this.width;
+    }
+
+
+
+}
