9517f7cf8d6d934643c21ba4e2820948429d2f49
Darek <dariusz.matacz11@gmail.com>
2022-11-07 15:10:36 +0100

    added ComplexNumber class

---
+++ src/main/java/pl/learning/complexOperations/ComplexNumber.java
@@ -0,0 +1,58 @@
+package pl.learning.complexOperations;
+
+public class ComplexNumber {
+
+    public static void main(String[] args) {
+        ComplexNumber one = new ComplexNumber(1.0, 1.0);
+        ComplexNumber number = new ComplexNumber(2.5, -1.5);
+        one.add(1, 1);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        one.subtract(number);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        number.subtract(one);
+        System.out.println("number.real= " + number.getReal());
+        System.out.println("number.imaginary= " + number.getImaginary());
+    }
+
+    private double real, imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void add(ComplexNumber parameter) {
+        this.real += parameter.real;
+        this.imaginary += parameter.imaginary;
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber parameter) {
+        this.real -= parameter.real;
+        this.imaginary -= parameter.imaginary;
+    }
+
+
+
+}
+
+
