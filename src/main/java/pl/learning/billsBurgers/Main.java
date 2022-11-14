f0060a5fc141cc5046bc625e54e9c19d7e8ebe66
Darek <dariusz.matacz11@gmail.com>
2022-11-14 23:51:29 +0100

    added Main class

---
+++ src/main/java/pl/learning/billsBurgers/Main.java
@@ -0,0 +1,23 @@
+package pl.learning.billsBurgers;
+
+public class Main {
+    public static void main(String[] args) {
+        Hamburger order1 = new Hamburger("white", 2, 1, 2, 2, 2 );
+        HealthyBurger order2 = new HealthyBurger(2, 1,1,1,0,1,true);
+        DeluxeHamburger order3 = new DeluxeHamburger("glutenfree", 2);
+
+        order1.order();
+        order1.pay();
+        System.out.println("");
+
+        order2.order();
+        order2.pay();
+        System.out.println("");
+
+        order3.order();
+        order3.pay();
+        System.out.println("");
+
+        System.out.println("The bill for all orders is " + (order1.price() + order2.price() + order3.price()) + "$.");
+    }
+}
